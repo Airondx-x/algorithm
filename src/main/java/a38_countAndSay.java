08ca91191e8f074801d8a9f90bdd6cf13168fe11
MEW <374684802@qq.com>
2019-12-28 17:14:36 +0800

    leetcode算法

--- src/main/java/a38_countAndSay.java
+++ src/main/java/a38_countAndSay.java
@@ -1,32 +1,26 @@
 public class a38_countAndSay {
-    String result = "";
 
     public String countAndSay(int n) {
-        test("1",1,n);
-        return result;
-    }
-
-    public void test(String num,int fir,int n){
-        if(fir==n)
-            result = num;
-        else {
-            String str = "" + num;
-            String str2 = "";
-            int count = 1;
-            for (int i = 0; i < str.length(); i++) {
-                if (i + 1 > str.length()-1 || str.charAt(i + 1) != str.charAt(i)) {
-                    str2 += (count+"")+(str.charAt(i)+"");
-                    count = 1;
-                } else
-                    count++;
-            }
-            test(str2, fir + 1, n);
-        }
+        int count = 1;
+        String curr = "1";
+        for(int c=2;c<=n;c++){
+            String res = "";
+           for (int i = 0; i < curr.length(); i++) {
+               if (i + 1 > curr.length() - 1 || curr.charAt(i + 1) != curr.charAt(i)) {
+                   res+= (count + "") + (curr.charAt(i) + "");
+                   count = 1;
+               } else {
+                   count++;
+               }
+           }
+           curr = res;
+       }
+       return curr;
     }
 
 
     public static void main(String[] args) {
         a38_countAndSay a38 = new a38_countAndSay();
-        a38.countAndSay(5);
+        System.out.println(a38.countAndSay(1));
     }
 }
